c66e7ce971dbc59d3b8540e05fcf27df1be32b24
Drew Jennings <andrew_jennings@ymail.com>
2021-12-03 07:00:50 +0000

    part 2 half complete

---
+++ day-3/part2.rb
@@ -0,0 +1,42 @@
+TEST_INPUT = '00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010'
+
+file = File.open('input.txt')
+input = file.read
+data_array = []
+input.lines do |line|
+    data_array << line.strip
+end
+p data_array
+
+data_array.first.length.times do |index|
+    if data_array.length == 2
+        p "INDEX = #{index}"
+        if data_array[0][index] != data_array[1][index]
+            data_array.delete_if { |binary| binary[index] == '0'}
+            break
+        end
+    end
+    zero_count = 0
+    one_count = 0
+    max = ''
+    data_array.each do |binary|
+        binary[index] == '0' ? zero_count += 1 : one_count += 1
+    end
+    zero_count > one_count ? max = '0' : max = '1'
+    p "MAX = #{max} === ZERO  #{zero_count}   ONE #{one_count}"
+    p '== DELETING == '
+    data_array.delete_if { |binary| binary[index] != max}
+    p data_array
+end
+p data_array.first.to_i(2)
