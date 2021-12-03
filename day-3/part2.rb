828c7a7f23ba47aa20f9af0d41880c09cc81ac28
Drew Jennings <andrew_jennings@ymail.com>
2021-12-03 07:07:32 +0000

    add correct answer for my input

--- day-3/part2.rb
+++ day-3/part2.rb
@@ -21,49 +21,56 @@ p data_array
 
 # OXYGEN
 
-# data_array.first.length.times do |index|
-#     if data_array.length == 2
-#         p "INDEX = #{index}"
-#         if data_array[0][index] != data_array[1][index]
-#             data_array.delete_if { |binary| binary[index] == '0'}
-#             break
-#         end
-#     end
-#     zero_count = 0
-#     one_count = 0
-#     max = ''
-#     data_array.each do |binary|
-#         binary[index] == '0' ? zero_count += 1 : one_count += 1
-#     end
-#     zero_count > one_count ? max = '0' : max = '1'
-#     p "MAX = #{max} === ZERO  #{zero_count}   ONE #{one_count}"
-#     p '== DELETING == '
-#     data_array.delete_if { |binary| binary[index] != max}
-#     p data_array
-# end
-# p data_array.first.to_i(2)
-
-# CO2
-
 data_array.first.length.times do |index|
     if data_array.length == 2
         p "INDEX = #{index}"
         if data_array[0][index] != data_array[1][index]
-            data_array.delete_if { |binary| binary[index] == '1'}
+            data_array.delete_if { |binary| binary[index] == '0'}
             break
         end
     end
     zero_count = 0
     one_count = 0
-    min = ''
+    max = ''
     data_array.each do |binary|
         binary[index] == '0' ? zero_count += 1 : one_count += 1
     end
-    zero_count < one_count ? min = '0' : min = '1'
-    p "MIN = #{min} === ZERO  #{zero_count}   ONE #{one_count}"
+    zero_count > one_count ? max = '0' : max = '1'
+    p "MAX = #{max} === ZERO  #{zero_count}   ONE #{one_count}"
     p '== DELETING == '
-    data_array.delete_if { |binary| binary[index] != min}
+    data_array.delete_if { |binary| binary[index] != max}
     p data_array
 end
 p data_array.first.to_i(2)
+
+# CO2
+
+# data_array.first.length.times do |index|
+#     if data_array.length == 2
+#         p "INDEX = #{index}"
+#         if data_array[0][index] != data_array[1][index]
+#             data_array.delete_if { |binary| binary[index] == '1'}
+#             break
+#         end
+#     end
+#     zero_count = 0
+#     one_count = 0
+#     min = ''
+#     data_array.each do |binary|
+#         binary[index] == '0' ? zero_count += 1 : one_count += 1
+#     end
+#     zero_count < one_count ? min = '0' : min = '1'
+#     p "MIN = #{min} === ZERO  #{zero_count}   ONE #{one_count}"
+#     p '== DELETING == '
+#     data_array.delete_if { |binary| binary[index] != min}
+#     p data_array
+# end
+# p data_array.first.to_i(2)
+
+
+# 3597 OXYGEN
+# 1389 CO2
+
+# TOTAL = 4996233
+
 
