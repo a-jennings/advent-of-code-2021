afb332e5244ced871a0b5da3d560f6bf247f7f64
Drew Jennings <andrew_jennings@ymail.com>
2021-12-10 05:35:21 +0000

    complete day 10 part 1

---
+++ day-10/part1.rb
@@ -0,0 +1,57 @@
+file = File.open('input.txt')
+input = file.read
+
+def line_check(line)
+    legal_chars = []
+    chars = line.chars
+    chars.each do |char|
+        case char
+        when '('
+            legal_chars.unshift(')')
+        when '['
+            legal_chars.unshift(']')
+        when '{'
+            legal_chars.unshift('}')
+        when '<'
+            legal_chars.unshift('>')
+        when ')'
+            if legal_chars[0] == ')'
+                legal_chars.delete_at(0)
+            else
+                return 3
+                break
+            end
+        when ']'
+            if legal_chars[0] == ']'
+                legal_chars.delete_at(0)
+            else
+                return 57
+                break
+            end
+        when '}'
+            if legal_chars[0] == '}'
+                legal_chars.delete_at(0)
+            else
+                return 1197
+                break
+            end
+        when '>'
+            if legal_chars[0] == '>'
+                legal_chars.delete_at(0)
+            else
+                return 25137
+                break
+            end
+        else
+            return 0
+        end
+    
+    end
+end
+
+arr = []
+input.lines do |line|
+    chars = line.strip
+    arr << line_check(line)
+end
+p arr.sum
