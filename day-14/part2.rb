ceb2237342a1246d1be1f765fca8f4e847081094
Drew Jennings <andrew_jennings@ymail.com>
2021-12-14 08:24:34 +0000

    complete day 14 part 2

--- day-14/part2.rb
+++ day-14/part2.rb
@@ -16,6 +16,62 @@ input.lines do |line|
     end
 end
 
-rules.each do |rule|
-    p rule
+new_rules = {}
+
+rules.each do |key, value|
+    new_rules[key] = [key[0] + value, value + key[1]]
+end 
+
+char_list = {}
+
+template.chars.each_with_index do |char, index|
+    polymer = char + (template[index+1].nil? ? "" : template[index+1])
+    break if polymer.length == 1
+    if char_list.has_key?(polymer)
+        char_list[polymer] += 1
+    else
+        char_list[polymer] = 1
+    end
 end
+
+40.times do
+    char_list_additions = {}
+
+    char_list.each do |key, value|
+        polymers = new_rules[key]
+        polymers.each do |polymer|
+            if char_list_additions.has_key?(polymer)
+                char_list_additions[polymer] += (1 * value)
+            else
+                char_list_additions[polymer] = (1 * value)
+            end
+        end
+    end
+    char_list = char_list_additions
+
+end
+
+char_tally = {}
+char_list.each do |key, value|
+    if char_tally.has_key?(key[0])
+        char_tally[key[0]] += value
+    else
+        char_tally[key[0]] = value
+    end
+
+    if char_tally.has_key?(key[1])
+        char_tally[key[1]] += value
+    else
+        char_tally[key[1]] = value
+    end
+    # p char_tally
+end
+
+final_tally = {}
+
+char_tally.each do |key, value|
+    final_tally[key] = (value / 2.0).ceil
+end
+
+puts "Answer: #{(final_tally.values.max - final_tally.values.min)}"
+puts "Execution time: #{(Time.now - start_time).to_f.round(6)}s"
